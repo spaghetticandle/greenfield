8713b8c967a8f153603e129b7a2e62db1ff3a62e
Violet Suber <violetmands@gmail.com>
2017-09-14 10:11:08 -0500

    (add) skeleton for moods component

---
+++ src/app/components/Moods.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Moods extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Moods</h1>
+      </div>
+    );
+  }
+}
