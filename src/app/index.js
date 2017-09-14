40278ab6b7e6772243068aabb28fecad173c28a9
Violet Suber <violetmands@gmail.com>
2017-09-14 06:41:01 -0500

    (include) all components

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Sidebar } from './components/Sidebar';
+import { Dashboard } from './components/Dashboard';
+import { Settings } from './components/Settings';
+import { Personality } from './components/Personality';
+import { Moods } from './components/Moods';
+import { NewEntry } from './components/NewEntry';
+import { Diary } from './components/Diary';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello!</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="main" className="col-8">
+            Main
+          </div>
+          <div className="sidebar" className="col-4">
+            <Sidebar />
+          </div>
+        </div>
       </div>
     );
   }
