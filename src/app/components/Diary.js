4424fbfe484748ffee9ce10b08e9bcb5fbc473fa
Violet Suber <violetmands@gmail.com>
2017-09-14 10:10:48 -0500

    (add) skeleton for diary component

---
+++ src/app/components/Diary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Diary extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Your Diary</h1>
+      </div>
+    );
+  }
+}
