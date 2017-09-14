82d88fc41a9dff92ec776cbdb2c72351014acbfb
Violet Suber <violetmands@gmail.com>
2017-09-14 10:11:30 -0500

    (add) skeleton for new entry component

---
+++ src/app/components/NewEntry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class NewEntry extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>New Diary Entry</h1>
+      </div>
+    );
+  }
+}
