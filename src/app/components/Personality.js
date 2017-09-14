4cd936bd622a8e13ed83c3cddf0a70e8f3c6e6e8
Violet Suber <violetmands@gmail.com>
2017-09-14 10:11:54 -0500

    (add) skeleton for personality profile component

---
+++ src/app/components/Personality.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Personality extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Personality Profile</h1>
+      </div>
+    );
+  }
+}
