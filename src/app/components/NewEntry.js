cbfaedb0e43c165885240b67cec9f537f60c06b6
Violet Suber <violetmands@gmail.com>
2017-09-19 16:21:33 -0500

    (pseudocode) API call

--- src/app/components/NewEntry.js
+++ src/app/components/NewEntry.js
@@ -1,4 +1,43 @@
 import React from 'react';
+import ToneAnalyzerV3 from 'watson-developer-cloud/tone-analyzer/v3';
+
+/*
+const tone_analyzer = new ToneAnalyzerV3({
+  username: '{username}',
+  password: '{password}',
+  version_date: '{version}'
+  headers: {
+    'X-Watson-Learning-Opt-Out': 'true'
+  }
+});
+
+const params = {
+  body: (input);
+}
+
+tone_analyzer.tone(params, function(error, response) {
+  if (error)
+    console.log('error:', error);
+  else
+    console.log(JSON.stringify(response, null, 2));
+  }
+);
+*/
+
+// Use the POST endpoint with plain text
+// Params needed: Body Body, Header Content-Type (text/plain)
+// URL: https://gateway.watsonplatform.net/tone-analyzer/api/v3/
+
+/*
+fetch("https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?sentences=false&version=2016-05-19", {
+  body: (input),
+  headers: {
+    Accept: 'application/json',
+    "Content-Type": 'text/plain'
+  },
+  method: 'POST'
+});
+*/
 
 export class NewEntry extends React.Component {
   render() {
