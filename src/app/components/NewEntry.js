a7157aa9f106d98355402f25223a20b91a7249ce
Violet Suber <violetmands@gmail.com>
2017-09-19 14:43:06 -0500

    (add) submit new entry form

--- src/app/components/NewEntry.js
+++ src/app/components/NewEntry.js
@@ -3,8 +3,17 @@ import React from 'react';
 export class NewEntry extends React.Component {
   render() {
     return (
-      <div className="header">
-        <h1>New Diary Entry</h1>
+      <div className="mainContainer">
+        <div className="header">
+          <h1>New Diary Entry</h1>
+        </div>
+        <div className="mainBody">
+          <form>
+            <label for="newDiaryEntry">Your New Diary Entry:</label>
+            <textarea className="form-control" id="newDiaryEntry"></textarea>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
