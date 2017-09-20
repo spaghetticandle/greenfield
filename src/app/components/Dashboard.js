aebabb69afcab8057110249c33bfeb0b363e763a
Violet Suber <violetmands@gmail.com>
2017-09-20 16:34:06 -0500

    (include) sidebar and other views in dashboard view

--- src/app/components/Dashboard.js
+++ src/app/components/Dashboard.js
@@ -3,8 +3,18 @@ import React from 'react';
 export class Dashboard extends React.Component {
   render() {
     return (
-      <div className="header">
-        <h1>Dashboard</h1>
+      <div className="row">
+        <div className="main" className="col-xl-10 col-lg-10 col-md-9 col-sm-9">
+          <Route path="/login" component={Login} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/personality" component={Personality} />
+          <Route path="/moods" component={Moods} />
+          <Route path="/newentry" component={NewEntry} />
+          <Route path="/diary" component={Diary} />
+        </div>
+        <div className="sidebar" className="col-xl-2 col-lg-2 col-md-3 col-sm-3">
+          <Sidebar />
+        </div>
       </div>
     );
   }
