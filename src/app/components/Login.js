ec0fdf8e4543f926a1d4433e0d87e53bcac4f00f
Violet Suber <violetmands@gmail.com>
2017-09-20 17:07:59 -0500

    (add) login and signup forms

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -3,7 +3,38 @@ import React from 'react';
 export class Login extends React.Component {
   render() {
     return (
-      <div>test</div>
+      <div className="loginContainer container">
+        <div className="row">
+          <div className="login col-6">
+            <h1>Login</h1>
+            <div className="loginForm">
+              <form>
+                <div className="form-group">
+                  <input type="loginUsername" className="form-control" id="loginUsername" placeholder="username" />
+                </div>
+                <div className="form-group">
+                  <input type="loginPassword" className="form-control" id="loginPassword" placeholder="password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Log In</button>
+              </form>
+            </div>
+          </div>
+          <div className="signup col-6">
+            <h1>Sign Up</h1>
+            <div className="signupForm">
+              <form>
+                <div className="form-group">
+                  <input type="signupUsername" className="form-control" id="signupUsername" placeholder="username" />
+                </div>
+                <div className="form-group">
+                  <input type="signupPassword" className="form-control" id="signupPassword" placeholder="password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Sign Up</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
