e5602c667b31d3ff032f379cfa07fa95eb9c6005
Violet Suber <violetmands@gmail.com>
2017-09-23 00:28:22 -0500

    (add) method for using fake data

--- src/app/components/Personality.js
+++ src/app/components/Personality.js
@@ -4,32 +4,39 @@ import axios from 'axios';
 export class Personality extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { params: {},
-  personality: {} };
+    this.state = { personality: {}
+    // params: {}
+    // personality: {} 
+    };
   }
   componentDidMount() {
-    axios.get('api/diary').then(result => {
-      const entries = result.data.map(entry => {
-        return entry.post;
-      }).join(' ');
-      const params = {
-        text: entries,
-        consumption_preferences: true,
-        raw_scores: true,
-        headers: {
-          'accept-language': 'en',
-          'accept': 'application/json'
-        }
-      }
-      this.setState({params: params});
-      axios.post('api/personality', this.state.params).
-        then(response => {
-          this.setState({personality: response.data});
-          console.log(this.state.personality);
-        }).catch(error => {
-          console.log(error);
-        });
+    axios.get('api/personality_analysis').then(result => {
+      this.setState({ personality: result.data });
     });
+
+    // Actual request not using fake data
+    // axios.get('api/diary').then(result => {
+    //   const entries = result.data.map(entry => {
+    //     return entry.post;
+    //   }).join(' ');
+    //   const params = {
+    //     text: entries,
+    //     consumption_preferences: true,
+    //     raw_scores: true,
+    //     headers: {
+    //       'accept-language': 'en',
+    //       'accept': 'application/json'
+    //     }
+    //   }
+    //   this.setState({params: params});
+    //   axios.post('api/personality', this.state.params).
+    //     then(response => {
+    //       this.setState({personality: response.data});
+    //       console.log(this.state.personality);
+    //     }).catch(error => {
+    //       console.log(error);
+    //     });
+    // });
   }
   render() {
     return (
@@ -38,7 +45,6 @@ export class Personality extends React.Component {
           <h1>Personality Profile</h1>
         </div>
         <div className="mainBody">
-          {JSON.stringify(this.state.personality)}
         </div>
       </div>
     );
