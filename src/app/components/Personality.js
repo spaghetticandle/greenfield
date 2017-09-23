df4e2b0279b297212e00dcc50bdb73255637f8e0
Violet Suber <violetmands@gmail.com>
2017-09-23 13:37:33 -0500

    (render) personality graph on page

--- src/app/components/Personality.js
+++ src/app/components/Personality.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+const BarChart = require('react-d3-components').BarChart;
+
 export class Personality extends React.Component {
   constructor(props) {
     super(props);
@@ -39,12 +41,25 @@ export class Personality extends React.Component {
     // });
   }
   render() {
+    const chartData = [{
+      label: 'Personality Profile',
+      values: [
+        {x: 'Openness', y: 0.9665868393457593 * 100},
+        {x: 'Conscientiousness', y: 0.027650414904379916 * 100},
+        {x: 'Extraversion', y: 0.3273222739414172 * 100},
+        {x: 'Agreeableness', y: 0.7287197035708 * 100},
+        {x: 'Emotional Range', y: 0.14439533987606717 * 100}
+      ]
+    }]
     return (
       <div className="mainContainer">
         <div className="header">
           <h1>Personality Profile</h1>
         </div>
         <div className="mainBody">
+          <div className="personalityChart">
+            <BarChart data={chartData} width={900} height={400} margin={{ top: 10, bottom: 50, left: 50, right: 10 }} />
+          </div>
         </div>
       </div>
     );
