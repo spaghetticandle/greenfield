a7c0a81c750cbc37feaeb8fa611a9bbdd084dd03
Violet Suber <violetmands@gmail.com>
2017-09-22 21:04:31 -0500

    (fix) routing

--- src/app/index.js
+++ src/app/index.js
@@ -2,27 +2,41 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-import { Sidebar } from './components/Sidebar';
+import { Login } from './components/Login';
 import { Dashboard } from './components/Dashboard';
+import { Sidebar } from './components/Sidebar';
 import { Settings } from './components/Settings';
 import { Personality } from './components/Personality';
 import { Moods } from './components/Moods';
 import { NewEntry } from './components/NewEntry';
 import { Diary } from './components/Diary';
-import { Login } from './components/Login';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <Route path="/" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
+        <div className="row">
+          <div className="main" className="col-xl-10 col-lg-10 col-md-9 col-sm-9">
+            <Switch>
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/settings" component={Settings} />
+              <Route path="/personality" component={Personality} />
+              <Route path="/moods" component={Moods} />
+              <Route path="/newentry" component={NewEntry} />
+              <Route path="/diary" component={Diary} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+          <div className="sidebar" className="col-xl-2 col-lg-2 col-md-3 col-sm-3">
+            <Sidebar />
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 
 render((
   <BrowserRouter>
