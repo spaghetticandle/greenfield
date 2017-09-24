40196af0d3d363ddd3b1bf0dee7f56a86e90aac9
Violet Suber <violetmands@gmail.com>
2017-09-23 23:18:25 -0500

    (isolate) login form

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -1,37 +1,52 @@
 import React from 'react';
 
 export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+    this.processForm = this.processForm.bind(this);
+    this.changeUsername = this.changeUsername.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+  }
+  changeUsername(event) {
+    const user = this.state.user;
+    user.username = event.target.value;
+    this.setState({ user });
+  }
+  changePassword(event) {
+    const user = this.state.user;
+    user.password = event.target.value;
+    this.setState({ user });
+  }
+  // Process the form
+  processForm(event) {
+    event.preventDefault();
+    console.log('name', this.state.user.username);
+    console.log('password', this.state.user.password);
+  }
   render() {
     return (
-      <div className="loginContainer container">
-        <div className="row">
-          <div className="login col-6">
-            <h1>Login</h1>
-            <div className="loginForm">
-              <form>
-                <div className="form-group">
-                  <input type="loginUsername" className="form-control" id="loginUsername" placeholder="username" />
-                </div>
-                <div className="form-group">
-                  <input type="loginPassword" className="form-control" id="loginPassword" placeholder="password" />
-                </div>
-                <button type="submit" className="btn btn-primary">Log In</button>
-              </form>
-            </div>
-          </div>
-          <div className="signup col-6">
-            <h1>Sign Up</h1>
-            <div className="signupForm">
-              <form>
-                <div className="form-group">
-                  <input type="signupUsername" className="form-control" id="signupUsername" placeholder="username" />
-                </div>
-                <div className="form-group">
-                  <input type="signupPassword" className="form-control" id="signupPassword" placeholder="password" />
-                </div>
-                <button type="submit" className="btn btn-primary">Sign Up</button>
-              </form>
-            </div>
+      <div className="mainContainer">
+        <div className="header">
+          <h1>Log In</h1>
+        </div>
+        <div className="mainBody">
+          <div className="loginForm">
+            <form onSubmit={this.processForm}>
+              <div className="form-group">
+                <input type="loginUsername" className="form-control" id="loginUsername" placeholder="username" onChange={this.changeUsername}/>
+              </div>
+              <div className="form-group">
+                <input type="loginPassword" className="form-control" id="loginPassword" placeholder="password" onChange={this.changePassword}/>
+              </div>
+              <button type="submit" className="btn btn-primary">Log In</button>
+            </form>
           </div>
         </div>
       </div>
